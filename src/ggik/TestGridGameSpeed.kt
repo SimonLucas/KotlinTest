85d8dda4498bc1a1f828beb90a10a8c747234f04
Simon Lucas <simonmarklucas@gmail.com>
2019-02-07 18:32:14 +0000

    Played with RHEA params

--- src/ggik/TestGridGameSpeed.kt
+++ src/ggik/TestGridGameSpeed.kt
@@ -19,18 +19,22 @@ fun main(args: Array<String>) {
             DoNothingAgent(game.doNothingAction()),
             // SimpleEvoAgent(),
             // SimpleEvoAgent(useShiftBuffer = false),
-            // SimpleEvoAgent(useMutationTransducer = false, sequenceLength = 20, nEvals = 10),
+            SimpleEvoAgent(useMutationTransducer = false, useShiftBuffer = false, sequenceLength = 20, nEvals = 5),
+            SimpleEvoAgent(useMutationTransducer = false, useShiftBuffer = true, sequenceLength = 20, nEvals = 5),
+            SimpleEvoAgent(useMutationTransducer = false, useShiftBuffer = true, sequenceLength = 5, nEvals = 20),
+            SimpleEvoAgent(useMutationTransducer = true, useShiftBuffer = true, sequenceLength = 5, nEvals = 20)
             // SimpleEvoAgent(useMutationTransducer = false, sequenceLength = 10, nEvals = 20),
             // SimpleEvoAgent(useMutationTransducer = false, sequenceLength = 5, nEvals = 40),
             // SimpleEvoAgent(repeatProb = 0.0),
             // SimpleEvoAgent(repeatProb = 0.5)
             // SimpleEvoAgent(repeatProb = 0.8),
             // SimpleEvoAgent(probMutation = 0.1, repeatProb = 0.8)
-            RandomAgent()
+            // RandomAgent()
 
     )
     val runner = GameRunner()
     runner.maxTicks = 500
+    runner.verbose = true
     val nGames = 10
     for (game in games) {
         for (agent in agents) {
