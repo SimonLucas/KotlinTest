45555b6fdc5902e41ae7ddbdeb18b61f4643f9a6
SImon Lucas <simonmarklucas@gmail.com>
2019-03-19 14:54:11 +0900

    Updated CityWars

--- src/games/citywars/CityWarsView.kt
+++ src/games/citywars/CityWarsView.kt
@@ -1,5 +1,6 @@
 package games.citywars
 
+import utilities.JEasyFrame
 import java.awt.Color
 import java.awt.Dimension
 import java.awt.Graphics
@@ -9,11 +10,9 @@ import javax.swing.JComponent
 fun main() {
 
     val game = CityWars()
-    val view = CityWarsView(game.board)
+    val view = CityWarsView(game)
 
-
-
-
+    JEasyFrame(view, "City Wars")
 
 }
 
@@ -25,35 +24,46 @@ class CityWarsView
 
 
 (// GridGame gridGame;
-        var grid: Grid) : JComponent() {
+        var game: CityWars) : JComponent() {
 
-    internal var cellSize = 20
+    internal var cellSize = 50
     internal var deadBlack = true
+
+    val colorMap = hashMapOf<Int,Color>(
+            0 to Color.white,
+            1 to Color.getHSBColor(0.35f, 1f, 1f),
+            2 to Color.black
+    )
 
     var gridLines = true
 
     public override fun paintComponent(g: Graphics) {
-        val n = grid.w * grid.h
-        for (i in 0 until n) {
-            val h = if (grid.getCell(i) == 0) 0.35f else 0.89f
-            g.color = Color.getHSBColor(h, 1f, 1f)
-            if (deadBlack && grid.getCell(i) == 0) g.color = Color.black
-            val x = cellSize * (i % grid.w)
-            val y = cellSize * (i / grid.w)
-            g.fillRect(x, y, cellSize, cellSize)
-        }
-        // paint faint gridlines separately
-        if (gridLines) {
-            g.color = Color(128, 128, 128, 128)
+        with (game) {
+            val grid = board
+            val n = grid.w * grid.h
             for (i in 0 until n) {
+
+                val cell = grid.getCell(i)
+                g.color = colorMap[cell]
                 val x = cellSize * (i % grid.w)
                 val y = cellSize * (i / grid.w)
-                g.drawRect(x, y, cellSize, cellSize)
+
+                g.fillRect(x, y, cellSize, cellSize)
+            }
+            // paint faint gridlines separately
+            if (gridLines) {
+                g.color = Color(128, 128, 128, 128)
+                for (i in 0 until n) {
+                    val x = cellSize * (i % grid.w)
+                    val y = cellSize * (i / grid.w)
+                    g.drawRect(x, y, cellSize, cellSize)
+                }
             }
         }
     }
 
     override fun getPreferredSize(): Dimension {
+        val grid = game.board
         return Dimension(cellSize * grid.w, cellSize * grid.h)
     }
 }
