ad4d6a80b942ddb83441031740b3d4dbc30d7864
SImon Lucas <simonmarklucas@gmail.com>
2020-04-02 22:02:13 +0100

    Update HelloWorld.kt

--- src/test/HelloWorld.kt
+++ src/test/HelloWorld.kt
@@ -1,5 +1,21 @@
 package test
 
+import games.citywars.showCityOutline
+
+
 fun main(args: Array<String>) {
-    print("Hello")
+    var tot = 0
+    val list = ArrayList<Int>()
+    for (i in 1 .. 10) {
+        // println(i)
+        tot += i
+        list.add(i)
+    }
+    println(tot)
+    println(list)
+    println(list.sum())
+
+
+
 }
+
